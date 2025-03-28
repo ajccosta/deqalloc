069495cd97c1595d3205c59a8bb473617f56d2d0
ajccosta <aj.costa@campus.fct.unl.pt>
2025-03-28 19:25:24 +0000

    added simple unit tests

--- examples/deqalloc/unit/unit_tests.cpp
+++ examples/deqalloc/unit/unit_tests.cpp
@@ -1,12 +1,36 @@
 #include <cstdlib>
 #include <iostream>
 #include <rapidcheck.h>
+#include <thread>
+#include "../libdeqalloc.cpp"
+
+template<class... Inputs>
+void run_multi_threaded(auto f,
+  Inputs... ins,
+  size_t nthreads = thread::hardware_concurrency()) {
+
+  std::vector<thread> threads(nthreads);
+  for (int i = 0; i < nthreads; i++) threads[i] = thread(f, ins...);
+  for (int i = 0; i < nthreads; i++) threads[i].join();
+}
 
 int main() {
-  rc::check("T", [](unsigned short n) {
+  rc::check("Single threaded malloc free", [](unsigned short n) {
     RC_PRE(n > 0);
-    auto sum = 0;
-    auto expected = 0;
-    RC_ASSERT(sum == expected);
+    void* ptr = xxmalloc(n);
+    xxfree(ptr);
+    RC_ASSERT((uintptr_t)ptr != 0);
+  });
+
+  rc::check("Multi threaded malloc free", [](unsigned short n) {
+    RC_PRE(n > 0);
+    auto f = [&](unsigned short n){
+      for(int i = 0; i < n; i++) {
+        void* ptr = xxmalloc(n);
+        xxfree(ptr);
+        RC_ASSERT((uintptr_t)ptr != 0);
+      }
+    };
+    run_multi_threaded<unsigned short>(f, n);
   });
 }
