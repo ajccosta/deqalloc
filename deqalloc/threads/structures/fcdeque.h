894b3663b32c804288093d4922c36cba362b08c8
Zak Kent <zkent@cs.cmu.edu>
2026-03-23 18:10:40 -0400

    Fix race in allocator

--- deqalloc/threads/structures/fcdeque.h
+++ deqalloc/threads/structures/fcdeque.h
@@ -9,28 +9,62 @@
 
 #include "../threadmanager.h"
 
-struct Allocator {
-  static constexpr size_t BLOCK_SIZE_LOG = 12;
-  using Base = FreelistHeap<BumpAlloc<BLOCK_SIZE_LOG, SizedMmapHeap>>;
-  static inline Base allocator;
-  static void *malloc(size_t sz) {
-    return allocator.malloc(sz);
-  }
-  static void free(void *ptr) {
-    allocator.free(ptr);
-  }
-};
-
+// Padding struct
 template<size_t N>
 struct Pad { char data[N]; };
 
+// Specializtion for zero: empty struct
 template<>
 struct Pad<0> {};
 
-template<typename T>
+// A concurrent deque based on flat combining
+template<typename T, size_t MaxThreads = max_threads>
 class FCDeque {
 public:
- struct alignas(64) Request {
+  FCDeque() :
+    heap{},
+    backing(Allocator<T>(heap)),
+    threadRequests(MaxThreads, Allocator<Request>(heap))
+  {}
+
+  void push_back(T value) {
+    threadRequests[thread_id()].set(value, Request::PushBack);
+    wait();
+  }
+
+  void push_front(T value) {
+    threadRequests[thread_id()].set(value, Request::PushFront);
+    wait();
+  }
+
+  std::optional<T> pop_back() {
+    size_t tid = thread_id();
+    threadRequests[tid].setStatus(Request::PopBack);
+    wait();
+    return threadRequests[tid].value;
+  }
+
+  std::optional<T> pop_front() {
+    size_t tid = thread_id();
+    threadRequests[tid].setStatus(Request::PopFront);
+    wait();
+    return threadRequests[tid].value;
+  }
+
+  std::pair<std::optional<T>, bool> pop_top() {
+    return {pop_front(), false};
+  }
+
+  auto pop_bottom() {
+    return pop_back();
+  }
+
+  void push_bottom(T val) {
+    push_back(val);
+  }
+
+private:
+  struct alignas(64) Request {
     enum Status {
       PushBack,
       PushFront,
@@ -46,11 +80,6 @@ public:
       this->value = value;
       setStatus(status);
     }
-    void set(Status status)
-    {
-      value.reset();
-      setStatus(status);
-    }
     void setStatus(Status status) {
       this->status.store(status, std::memory_order_release);
     }
@@ -64,7 +93,8 @@ public:
       return !isComplete(mo);
     }
     void reset() {
-      set(Complete);
+      value.reset();
+      setStatus(Complete);
     }
     void complete(T value) {
       set(value, Complete);
@@ -81,7 +111,36 @@ public:
 
   static_assert(sizeof(Request) % 64 == 0, "Request must be cache-line sized");
 
-  FCDeque() : threadRequests(max_threads) {}
+  static constexpr size_t BLOCK_SIZE_LOG = 12;
+  using Heap = FreelistHeap<BumpAlloc<BLOCK_SIZE_LOG, SizedMmapHeap>>;
+
+  template<typename V>
+  struct Allocator {
+    using value_type = V;
+    Heap &heap;
+
+    Allocator(Heap &heap) noexcept : heap(heap) {}
+
+    template<typename U>
+    Allocator(const Allocator<U>& other) noexcept : heap(other.heap) {}
+
+    V *allocate(size_t sz) {
+      return static_cast<V*>(heap.malloc(sz * sizeof(V)));
+    }
+    void deallocate(V *ptr, size_t) {
+      heap.free(ptr);
+    }
+
+    template<typename U>
+    bool operator==(const Allocator<U>& other) const noexcept {
+      return &heap == &other.heap;
+    }
+
+    template<typename U>
+    bool operator!=(const Allocator<U>& other) const noexcept {
+      return !(*this == other);
+    }
+  };
 
   void combine() {
     size_t threadCount = num_threads();
@@ -110,7 +169,7 @@ public:
         case Request::PopBack: {
           if (backing.empty()) {
             request.reset();
-          } else { 
+          } else {
             request.complete(backing.back());
             backing.pop_back();
           }
@@ -134,49 +193,9 @@ public:
     }
   }
 
-  void push_back(T value) {
-    threadRequests[thread_id()].set(value, Request::PushBack);
-    wait();
-  }
-
-  void push_front(T value) {
-    threadRequests[thread_id()].set(value, Request::PushFront);
-    wait();
-  }
-
-  std::optional<T> pop_back() {
-    size_t tid = thread_id();
-    threadRequests[tid].set(Request::PopBack);
-    wait();
-    return threadRequests[tid].value;
-  }
-
-  std::optional<T> pop_front() {
-    size_t tid = thread_id();
-    threadRequests[tid].set(Request::PopFront);
-    wait();
-    return threadRequests[tid].value;
-  }
-
-  std::pair<std::optional<T>, bool> pop_top() {
-    return {pop_front(), false};
-  }
-
-  auto pop_bottom() {
-    return pop_back();
-  }
-
-  void push_bottom(T val) {
-    push_back(val);
-  }
-
-  static constexpr size_t BLOCK_SIZE_LOG = 12;
-
-  template<typename V>
-  using STLAllocator = HL::STLAllocator<V, Allocator>;
-
-  std::deque<T, STLAllocator<T>> backing;
+  Heap heap;
+  std::deque<T, Allocator<T>> backing;
   std::mutex mutex;
-  std::vector<Request, STLAllocator<Request>> threadRequests;
+  std::vector<Request, Allocator<Request>> threadRequests;
 
 };
